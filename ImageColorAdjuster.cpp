de9ab5b01e77e6dc55836e3b07966a3cce2ac4f0
kevink0623 <87101697+kevink0623@users.noreply.github.com>
2024-04-16 00:01:31 +0800

    Update ImageColorAdjuster.cpp

--- ImageColorAdjuster.cpp
+++ ImageColorAdjuster.cpp
@@ -16,8 +16,11 @@ cv::Mat ImageColorAdjuster::RemapColorCurve1(const cv::Mat& image)
 			float Y = 0.299 * pcolor[2] + 0.587 * pcolor[1] + 0.114 * pcolor[0];
 			float Cb = 0.568 * (pcolor[0] - Y) + 128;
 			float Cr = 0.713 * (pcolor[2] - Y) + 128;
-
+			
+			Y /= 255;
 			float newY = pow(Y, 0.85);
+			newY *= 255;
+			
 
 			/*uchar newR = max((uchar)0, min((uchar)255, (uchar)(newY + 1.371 * (Cr - 128))));
 			uchar newG = max((uchar)0, min((uchar)255, (uchar)(newY - 0.698 * (Cr - 128) - 0.336 * (Cb - 128))));
@@ -59,15 +62,27 @@ cv::Mat ImageColorAdjuster::RemapColorCurve2(const cv::Mat& image)
 		for (int w = 0; w < width; ++w) {
 			cv::Vec3b pcolor = image.at<cv::Vec3b>(h, w);
 
+			/*float r = pcolor[2] / 255;
+			float g = pcolor[1] / 255;
+			float b = pcolor[0] / 255;
+
+			float Y = 0.299 * r + 0.587 * g + 0.114 * b;
+			float Cb = 0.568 * (b - Y) + 128;
+			float Cr = 0.713 * (r - Y) + 128;*/
+
 			float Y = 0.299 * pcolor[2] + 0.587 * pcolor[1] + 0.114 * pcolor[0];
 			float Cb = 0.568 * (pcolor[0] - Y) + 128;
 			float Cr = 0.713 * (pcolor[2] - Y) + 128;
 
-			float newY = pow(Y, 1.50);
+			float newY = 255.0f * pow( Y / 255.0f, 1.50);
 
-			uchar newR = max((uchar)0, min((uchar)255, (uchar)(newY + 1.371 * (Cr - 128))));
+			/*uchar newR = max((uchar)0, min((uchar)255, (uchar)(newY + 1.371 * (Cr - 128))));
 			uchar newG = max((uchar)0, min((uchar)255, (uchar)(newY - 0.698 * (Cr - 128) - 0.336 * (Cb - 128))));
-			uchar newB = max((uchar)0, min((uchar)255, (uchar)(newY + 1.732 * (Cb - 128))));
+			uchar newB = max((uchar)0, min((uchar)255, (uchar)(newY + 1.732 * (Cb - 128))));*/
+
+			uchar newR = cv::saturate_cast<uchar>(newY + 1.371 * (Cr - 128));
+			uchar newG =cv::saturate_cast<uchar>(newY - 0.698 * (Cr - 128) - 0.336 * (Cb - 128));
+			uchar newB = cv::saturate_cast<uchar>(newY + 1.732 * (Cb - 128));
 
 			newImage.at<cv::Vec3b>(h, w) = cv::Vec3b(newB, newG, newR);
 
